4cb9f7a0b2e3195e1f16b5c2eb9c127489577c6f
minhchau1112 <tnmchau1211@gmail.com>
2023-12-05 00:34:44 +0700

    Updated layout for user and folder structure

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
+import express from "express";
+import { engine } from "express-handlebars";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -8,23 +8,53 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = 8888
 const app = express()
 
-app.engine('hbs', engine({
-    extname: 'hbs',
-    defaultLayout: 'main',
+app.use(
+  express.urlencoded({
+      extended: true
+  })
+);
+
+app.engine("hbs", engine({
+    extname: "hbs",
+    defaultLayout: "main",
     helpers: {
       format_number(val) {
-        return numeral(val).format('0,0');
+        return numeral(val).format("0,0");
+      },
+      equal(value1, value2) {
+        return value1 == value2;
       }
     }
 }));
 
-app.set('view engine', 'hbs');
-app.set('views',  __dirname + '/views/user/');
+app.set("view engine", "hbs");
+app.set("views", "./views/user");
 
-app.use('/static', express.static('static'));
-app.get('/', function (req, res) {
-    res.render('home');
-})
+app.use("/static", express.static("static"));
+
+app.get("/", function(req, res) {
+  res.render("home", {
+      user: false,
+      type: "home",
+      userName: "Họ và tên"
+  });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+      user: true,
+      type: "contact",
+      userName: "Họ và tên"
+  });
+});
+
+app.get("/food", function(req, res) {
+  res.render("food", {
+      user: true,
+      type: "food",
+      userName: "Họ và tên"
+  });
+});
 
 app.listen(port, function serverStartedHandler() {
     console.log(`Door-rush server is running at http://localhost:${port}`);
