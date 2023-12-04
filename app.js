e5160fb67aaf56b911cf64d791baf162e865ea1a
minhchau1112 <tnmchau1211@gmail.com>
2023-12-04 14:56:51 +0700

    Render NavigationBar

--- app.js
+++ app.js
@@ -8,9 +8,23 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = 8888
 const app = express()
 
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    helpers: {
+      format_number(val) {
+        return numeral(val).format('0,0');
+      }
+    }
+}));
+
+app.set('view engine', 'hbs');
+app.set('views',  __dirname + '/views/user/');
+
+app.use('/static', express.static('static'));
 app.get('/', function (req, res) {
-    res.send("Hello Door-rush");
-  })
+    res.render('home');
+})
 
 app.listen(port, function serverStartedHandler() {
     console.log(`Door-rush server is running at http://localhost:${port}`);
