dbacc6d1cadc412a97bbca0cf88b6feea669ccbf
dinhhuyhuy <130577756+dinhhuyhuy@users.noreply.github.com>
2023-12-06 20:49:45 +0700

    Update food.route.js

--- routes/user/food.route.js
+++ routes/user/food.route.js
@@ -1,5 +1,10 @@
 import express from "express";
 import foodsController from "../../controllers/user/foods.controller.js";
 const router = express.Router();
+
 router.get("/", foodsController.index);
+router.get("/:shop-name", foodsController.shop);
+router.get("/:shop-name/:id", foodsController.foodDetail);
+
 export default router;
+
