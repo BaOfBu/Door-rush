1b1b348f736cf3c6fb179e5563b1f3e526592637
TBin <TBinTB0003@gmail.com>
2023-12-06 11:57:05 +0700

    Add the section 1 of the food-detail page

--- routes/user/food.route.js
+++ routes/user/food.route.js
@@ -2,4 +2,6 @@ import express from "express";
 import foodsController from "../../controllers/user/foods.controller.js";
 const router = express.Router();
 router.get("/", foodsController.index);
+router.get("/shop", foodsController.shop);
+router.get("/shop/:id", foodsController.foodDetail);
 export default router;
