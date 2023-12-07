c43a791096f7599d11c53494d068dafe236315e9
TBin <TBinTB0003@gmail.com>
2023-12-07 12:40:37 +0700

    Change the logic of the food detail page

--- controllers/user/foods.controller.js
+++ controllers/user/foods.controller.js
@@ -6,6 +6,7 @@ const index = function (req, res) {
         userName: "Họ và tên"
     });
 };
+
 // [GET]/foods/{{shop_name}}
 const shop = function (req, res) {
     res.render("user/shop", {
@@ -14,16 +15,35 @@ const shop = function (req, res) {
         userName: "Họ và tên"
     });
 };
-// [GET]/foods/{{shop_name}}/{{foodID}}
+// [GET]/foods/{{shop_id}}/{{foodID}}
 const foodDetail = async function (req, res) {
+    // Get the params from the route
+    const shopId = req.params.shop || 0;
     const foodId = req.params.id || 0;
+    // Handle the problem
+    if (!shopId) {
+        return res.redirect("/");
+    }
     if (!foodId) {
         return res.redirect("/shop");
     }
-    let countRating = [1, 2, 3, 4, 5, 6];
+    // Get the value for website
+    let foodName = foodId;
+    let shopName = shopId;
+    let foodPrice = "55.000 VNĐ";
+    let userRatingAvg = 0;
+    let userRatingAll = [1, 2, 3, 4, 5, 6];
+    let typeOfFood = ["Loại 1", "Loại 2", "Loại 3"];
     res.render("user/food-detail.hbs", {
-        countRating: countRating,
+        // Data of page
+        shopName: shopName,
+        foodName: foodName,
+        foodPrice: foodPrice,
+        userRatingAvg: userRatingAvg > 0,
+        userRatingAll: userRatingAll,
+        // data of header
         user: false,
+        typeOfFood: typeOfFood,
         type: "food",
         userName: "Họ và tên"
     });
