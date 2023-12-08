1e14eb98c877aee00ce2499ab6bac2e8e2cab195
TBin <TBinTB0003@gmail.com>
2023-12-09 01:42:52 +0700

    Change

--- static/js/user/food-detail/index.js
+++ static/js/user/food-detail/index.js
@@ -16,3 +16,7 @@ const swiper = new Swiper(".food-detail-card-review-content", {
         prevEl: ".swiper-button-prev"
     }
 });
+document.getElementById("submitFormButton").addEventListener("click", function () {
+    document.getElementById("my-form").style.display = "none";
+    document.getElementById("thank-you-form").style.display = "block";
+});
