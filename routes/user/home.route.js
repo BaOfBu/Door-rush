a9dbe04250ae98ad289060bd3ee8505514d36780
minhchau1112 <tnmchau1211@gmail.com>
2023-12-11 01:35:24 +0700

    Fix bug

--- routes/user/home.route.js
+++ routes/user/home.route.js
@@ -1,7 +1,14 @@
 import express from "express";
 import homeController from "../../controllers/user/home.controller.js";
+import Account from "../../models/accountModel.js";
+
 const router = express.Router();
 
 router.get("/", homeController.index);
+
+router.get("/test", async function (req, res, next) {
+    const account = await Account.find();
+    console.log(account[0].username);
+});
 
 export default router;
