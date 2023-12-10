f4acc4035c1a46ed3580365e7b04faf57fb20799
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2023-12-10 18:29:19 +0700

    login/sign up structure update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { engine } from "express-handlebars";
+import session from "express-session";
 import path from "path";
 import hbs_sections from "express-handlebars-sections";
 import { dirname } from "path";
@@ -8,6 +9,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 import userRoutes from "./routes/user/index.route.js";
 import adminRoutes from "./routes/admin/index.route.js";
+
+import auth from "./middleware/auth.mdw.js";
+
 
 const port = 8888;
 const app = express();
@@ -46,6 +50,24 @@ app.use("/static", express.static("static"));
 
 app.use("/", userRoutes);
 app.use("/admin", adminRoutes);
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+}));
+
+app.use(function (req, res, next) {
+    if (typeof (req.session.auth) === 'undefined') {
+      req.session.auth = false;
+    }
+  
+    res.locals.auth = req.session.auth;
+    res.locals.authUser = req.session.authUser;
+    next();
+});
 
 app.listen(port, function serverStartedHandler() {
     console.log(`Door-rush server is running at http://localhost:${port}`);
