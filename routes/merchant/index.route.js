bcaed9869c04f23a74bc76f6d4eff4d07187eb26
minhchau1112 <tnmchau1211@gmail.com>
2023-12-13 01:03:30 +0700

    Updated library

--- routes/merchant/index.route.js
+++ routes/merchant/index.route.js
@@ -1,7 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-// Middleware to apply default settings to the response locals
 function setDefaultLayoutAndPartials(req, res, next) {
     res.locals.layout = "merchant/layouts/main";
     next();
