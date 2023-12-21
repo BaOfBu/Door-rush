e0319d0e4e285dc56430bfeffbf3019bd6b4725b
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2023-12-21 11:09:49 +0700

    register js handle

---
+++ static/js/user/account/index.js
@@ -0,0 +1,45 @@
+$("#frmRegister").on("submit",function(e){
+  e.preventDefault();
+  var username = $("#txtUsername").val();
+  var password = $("#txtPassword").val();
+  var conpassword = $("#txtConfirm").val();
+  var email = $("#txtEmail").val();
+  var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+  var flag = false;
+
+  console.log(password);
+  console.log(conpassword);
+  if (username.length === 0 || username === null) {
+    alert('Please input username.');
+    return;
+  }
+  if(password !== conpassword){
+    alert('Password and confirm password do not match.');
+    return;
+  }
+  if(!pattern.test(email)){
+    alert('Invalid email address.');
+    return;
+  }
+  $.getJSON(`/account/is-available-user`,{username:username}, function (data) {
+    console.log(data);
+    if (data === false) {
+      alert('Username is already taken.');
+    } else {
+      flag = true;
+    }
+  });
+
+  $.getJSON(`/account/is-available-email`,{email:email}, function (data) {
+    console.log(data);
+    if (data === false) {
+      alert('Email is already taken.');
+    } else {
+      flag = true;
+    }
+  });
+  
+  if(flag === false){
+    $('#frmRegister').off('submit').submit();
+  }
+})
