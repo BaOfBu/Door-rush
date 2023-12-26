637085469315f9986b580bfcf9a9eadc4e57bb7b
N2TS-Dolphin <sonsung2003@gmail.com>
2023-12-27 00:11:36 +0700

    Update homepage admin

--- controllers/admin/home.controller.js
+++ controllers/admin/home.controller.js
@@ -1,6 +1,12 @@
-const index = function (req, res) {
+import Account from "../../models/accountModel.js";
+
+const index = async function (req, res) {
+    const CountByUser = await Account.countDocuments({role: "User"});
+    const CountByMerchant = await Account.countDocuments({role: "Merchant"});
     res.render("admin/home", {
-        type: "home"
+        type: "home",
+        countUser: CountByUser,
+        countMerchant: CountByMerchant
     });
 };
 export default { index };
