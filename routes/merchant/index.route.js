fe5b6128f3eb9af1a5ef8cfe947f7ec56ba54d85
minhchau1112 <tnmchau1211@gmail.com>
2023-12-27 13:14:01 +0700

    Draw side-bar for Merchant UI

--- routes/merchant/index.route.js
+++ routes/merchant/index.route.js
@@ -1,11 +1,15 @@
 import express from "express";
 const router = express.Router();
 
+import homeRoutes from "./home.route.js";
+
 function setDefaultLayoutAndPartials(req, res, next) {
     res.locals.layout = "merchant/layouts/main";
     next();
 }
 
 router.use(setDefaultLayoutAndPartials);
+
+router.use("/", homeRoutes);
 
 export default router;
