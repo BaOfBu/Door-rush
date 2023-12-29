668b71495f8f7ba4c682d35d583c9dc0c0f98a5c
TBin <TBinTB0003@gmail.com>
2023-12-30 01:46:34 +0700

    Update the layout for validate shop

---
+++ static/js/admin/manage-shop.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var currentDate = moment();
+    $('input[name="daterange"]').daterangepicker({
+        startDate: currentDate,
+        endDate: currentDate,
+        locale: {
+            cancelLabel: "Clear"
+        }
+    });
+    $('input[name="daterange"]').on("apply.daterangepicker", function (ev, picker) {
+        $(this).val(picker.startDate.format("DD/MM/YYYY") + " - " + picker.endDate.format("DD/MM/YYYY"));
+    });
+
+    $('input[name="daterange"]').on("cancel.daterangepicker", function (ev, picker) {
+        $(this).data("daterangepicker").setStartDate(currentDate);
+        $(this).data("daterangepicker").setEndDate(currentDate);
+        $(this).val(currentDate.format("MM/DD/YYYY") + " - " + currentDate.format("MM/DD/YYYY"));
+    });
+});
