48179becd4fd5937cd072f10e2a71cb7344fe701
TBin <TBinTB0003@gmail.com>
2023-12-29 20:37:36 +0700

    Finish the voucher page for admin

--- app.js
+++ app.js
@@ -86,7 +86,6 @@ app.use(function (req, res, next) {
     if (typeof req.session.auth === "undefined") {
         req.session.auth = false;
     }
-
     res.locals.auth = req.session.auth;
     res.locals.authUser = req.session.authUser;
     next();
