bb6f22f50dd3c240854804b43642cca4cc4a39a0
TBin <TBinTB0003@gmail.com>
2023-12-31 12:04:22 +0700

    Add the date and index for the validate shop page

--- controllers/admin/validate-shop.controller.js
+++ controllers/admin/validate-shop.controller.js
@@ -2,17 +2,20 @@ import MerchantService from "../../services/admin/merchant.service.js";
 // [GET]/admin/validate-shop?page=
 const index = async function (req, res) {
     const page = req.query.page || 1;
-    const limit = 6;
+    const limit = 5;
     const offset = (page - 1) * limit;
-    const merchant = await MerchantService.findPending(offset, limit);
+    const merchant = await MerchantService.findPending(offset, limit).populate("category");
     const totalCount = await MerchantService.countPending();
     const nPages = Math.ceil(totalCount / limit);
-    const merchantArray = merchant.map(merchant => {
+    const merchantArray = merchant.map((merchant, index) => {
         return {
             username: merchant.username,
             id: merchant._id,
             username: merchant.username,
-            name: merchant.name
+            name: merchant.name,
+            pageIndex: Number(offset) + Number(index) + 1,
+            timeRegister: new Date(merchant.timeRegister).toLocaleDateString("en-GB"),
+            categories: merchant.category.map(category => category.name).join(", ")
         };
     });
     res.render("admin/validate-shop", {
