eb6f812a9771498a3035f6fb1f511fb880afebe6
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2024-01-08 00:38:25 +0700

    chat update

--- controllers/merchant/chats.controller.js
+++ controllers/merchant/chats.controller.js
@@ -1,7 +1,12 @@
+import conversationService from "../../services/merchant/chat.service.js";
+
 const index = async function (req, res) {
+    var conversation = await conversationService.findConversationByMerchantId(req.session.authUser._id);
+    console.log("conversations: ",conversation);
     res.render("merchant/chat", {
         type: "chats",
-        userId: req.session.authUser._id
+        userId: req.session.authUser._id,
+        conversations: conversation,
     });
 };
 
--- services/merchant/chat.service.js
+++ services/merchant/chat.service.js
@@ -1,11 +1,21 @@
 import { ObjectId } from "mongodb";
 import Conversation from "../../models/conversationModel.js";
 import Message from "../../models/messageModel.js";
-import e from "express";
+import User from "../../models/userModel.js";
+import Merchant from '../../models/merchantModel.js';
 
 export default {
-    findConversationByMerchantId(merchantId) {
-        return Conversation.findOne({ merchantId: merchantId });
+    async findConversationByMerchantId(merchantId) {
+        var res = await Conversation.find({ merchantId: merchantId });
+        var result = [];
+        for (let i = 0; i < res.length; i++) {
+            var user = await User.findById(res[i].userId);
+            result.push({
+                userId: user._id,
+                username: user.username,
+            });
+        }
+        return result;
     },
     async findConversation(userId, merchantId) {
         userId = new ObjectId(userId);
