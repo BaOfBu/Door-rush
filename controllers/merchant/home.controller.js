4416ee14cdf5ee9d1d4bb95c9a0f6b132fa978c2
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2024-01-07 16:47:31 +0700

    update home merchant (multer avatar changing)

--- controllers/merchant/home.controller.js
+++ controllers/merchant/home.controller.js
@@ -1,6 +1,42 @@
+import multer from "multer";
+import Profile from "../../services/user/profile.service.js";
+
 const index = function (req, res) {
     res.render("merchant/home", {
         type: "home"
     });
 };
-export default { index };
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, process.cwd() + '/static/images/user/account/');
+    },
+    filename: function (req, file, cb) {
+        const userID = req.session.authUser._id;
+        const filename = userID + '.' + file.originalname.split('.').pop();
+        req.body.image = "/static/images/user/account/" + filename;
+        console.log(filename);
+        cb(null, filename);
+    }
+});
+const upload = multer({ storage: storage });
+
+const uploadAvatar = async function(req, res) {
+    console.log("Đã vô upload roi ne");
+    const userID = req.session.authUser._id;
+    console.log("userID: ", userID);
+    upload.single('image')(req, res, async function (err) {
+        if (err) {
+            console.error("error: ", err);
+            return res.status(500).json({ error: 'Error during upload.' });
+        } else {
+            console.log("file name: ", req.body.image);
+            const update = await Profile.updateMerchantInfo(userID, { image: req.body.image });
+            console.log("Đã up ảnh thành công, ", update);
+            //return res.json({ success: true, image: req.body.image });
+            req.session.authUser.image = req.body.image;
+            return res.redirect("/merchant/home");
+        }
+    });
+}
+export default {uploadAvatar, index };
