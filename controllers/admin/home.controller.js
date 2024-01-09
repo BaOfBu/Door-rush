cf35226b95b2251fbc33a31e283b5de41717e27f
N2TS-Dolphin <117240825+N2TS-Dolphin@users.noreply.github.com>
2024-01-09 19:31:06 +0700

    Update admin home.controller.js

--- controllers/admin/home.controller.js
+++ controllers/admin/home.controller.js
@@ -1,5 +1,12 @@
-const index = function (req, res) {
+import Account from "../../models/accountModel.js";
+
+const index = async function (req, res) {
+    const user = await Account.countDocuments({role: "User", status: "active"});
+    const merchant = await Account.countDocuments({role: "Merchant", status: "active"});
+
     res.render("admin/home", {
+        user: user,
+        merchant: merchant,
         type: "home"
     });
 };
